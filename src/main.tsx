6afdbcf82677ac52b7975fecb31dbbf24011ef51
hijuliancode <github-hijuliancode.designer777@passmail.com>
2026-02-04 15:29:39 -0500

    feat: add react-router and set up basic routing
    - Add react-router dependency to package.json
    - Wrap app with BrowserRouter in main.tsx
    - Add Routes and Route for root path
    - Add 404 error image asset for future use

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { ApolloProvider } from "@apollo/client/react";
 import { client } from "./graphql/client.ts";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </ApolloProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
